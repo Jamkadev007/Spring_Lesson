db56f4ddee88b3bd2ca743a6def8b1f89f9ac698
Jamkadev007 <jamshidbekbekmetov@gmail.com>
2024-05-16 21:20:36 +0500

    Commit 5

--- src/main/java/org/example/jpaspringlesson/dto/CarServise.java
+++ src/main/java/org/example/jpaspringlesson/dto/CarServise.java
@@ -2,8 +2,13 @@ package org.example.jpaspringlesson.dto;
 
 public class CarServise {
 
-//    uyguydflh
-//    ythagdfgdnxnnxnnxnxxnxnxnxn
+    private String name;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
